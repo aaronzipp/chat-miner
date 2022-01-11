416851851e8c4ab413779cf767a97c29a2b476ac
Aaron Zipp <aaronzipp@protonmail.com>
2022-01-11 16:55:40 +0100

    Rewrite wa_parser, now automatic format detection

--- wa_parser.py
+++ wa_parser.py
@@ -1,85 +1,186 @@
-import re
 import pandas as pd
+from parse import compile
+
+from typing import (
+    Callable,
+    Dict,
+    Optional,
+    List,
+    NewType,
+    Union,
+    Tuple,
+)
+
+# Types
+FilePath = NewType('FilePath', str)
 
 
-DATEREGEX = {
-    'dd/mm/yy':
-        r'^([0-2][0-9]|(3)[0-1][0-9])(\/)(([0-1][0-9])|((1)[0-2]))(\/)(\d{2}|\d{4})',
-    'dd/mm/yyyy':
-        r'^([0-2][0-9]|(3)[0-1][0-9])(\/)(([0-1][0-9])|((1)[0-2]))(\/)(\d{4}|\d{4})',
-    'mm/dd/yy':
-        r'^(([0-9])|((1)[0-2]))(\/)([0-2][0-9]|(3)[0-1]|[0-9])(\/)(\d{2}|\d{4})',
-    'dd.mm.yy':
-        r'^([0-2][0-9]|(3)[0-1][0-9])(\.)(([0-1][0-9])|((1)[0-2]))(\.)(\d{2}|\d{4})',
-    'dd.mm.yyyy':
-        r'^([0-2][0-9]|(3)[0-1][0-9])(\.)(([0-1][0-9])|((1)[0-2]))(\.)(\d{4}|\d{4})',
-    'mm.dd.yy':
-        r'^(([0-9])|((1)[0-2]))(\.)([0-2][0-9]|(3)[0-1]|[0-9])(\.)(\d{2}|\d{4})'
-}
+VIEW_ONCE_FLAG = "<View once>"
+MEDIA_FLAGS = ["<Media omitted>", "<Medien ausgeschlossen>"]
 
-TIMEREGEX = {
-    '24-hh:mm':
-        r'([0-9][0-9]):([0-9][0-9])',
-    '24-hh:mm:ss':
-        r'([0-9][0-9]):([0-9][0-9]):([0-9][0-9])',
-    '12-hh-mm':
-        r'(1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm])'
-}
+TIME_FORMAT = "{time:tt}"
+MESSAGE_FORMAT = "{author}: {message}"
+
+MAX_HOUR = 24
+MAX_MONTH = 12
+
+DATE_FORMATS = ["{day:d}.{month:d}.{year:d}",
+                "{day:d}/{month:d}/{year:d}"]
 
 
-def get_df_from_chatlog(filepath, dateformat='mm/dd/yy',
-                        timeformat='24-hh:mm'):
+DATE_AND_TIME = [f"{date_format}, {TIME_FORMAT}"
+                 for date_format in DATE_FORMATS]
 
-    def get_message_metadata(line):
-        date = re.search(DATEREGEX[dateformat], line).group(0)
-        time = re.search(TIMEREGEX[timeformat], line).group(0)
-        author = get_author_from_line(line)
-        return date, time, author
+FORMATS = [f"{date_and_time} - {MESSAGE_FORMAT}"
+           for date_and_time in DATE_AND_TIME]
 
-    def get_author_from_line(line):
-        patterns = [
-            r'- ([+])([0-9 ]+)(:)',
-            r'- ([\w]+):',
-            r'- ([\w]+[\s]+[\w]+):',
-            r'- ([\w]+[\s]+[\w]+[\s]+[\w]+):',
-        ]
-        pattern = '|'.join(patterns)
-        res = re.search(pattern, line)
-        return re.sub(r'|\-|\:', '', res.group(0)).strip() if res else 'System'
+date_dot, date_slash = [compile(date_and_time).search
+                        for date_and_time in DATE_AND_TIME]
 
-    assert filepath.endswith('.txt'),\
-        "Wrong file type: Specified file does not end with .txt"
 
-    with open(filepath, encoding="utf-8") as f:
-        parsed_chat = []
-        message_buffer = []
-        date, time, author = None, None, None
-        for line in f:
-            line = line.strip()
-            if re.match(DATEREGEX[dateformat], line):
-                if message_buffer:
-                    parsed_chat.append({
-                        'date': date,
-                        'time': time,
-                        'author': author,
-                        'message': ' '.join(message_buffer).strip()
-                    })
+def _parse_message(
+    message_lines: List[str],
+    parser: Callable,
+    max_hour: int = MAX_HOUR,
+) -> Dict:
 
-                message_buffer.clear()
-                date, time, author = get_message_metadata(line)
-                if author != 'System':
-                    message_buffer.append(line.split('-', 1)[1].split(':', 1)[1])
-                else:
-                    message_buffer.append(line.split('-', 1)[1])
-            else:
-                message_buffer.append(line)
+    message = "".join(message_lines).strip("\n")
+    parsed_message_dict = parser(message).named
+    if parsed_message_dict["year"] < 1000:
+        parsed_message_dict["year"] += 2000
+    return parsed_message_dict
 
-    df = pd.DataFrame(parsed_chat)
-    df = df[df['author'] != 'System']
-    df['datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'],
-                                    infer_datetime_format=True)
-    df['weekday'] = df['datetime'].dt.day_name()
-    df['words'] = df['message'].apply(lambda s: len(s.split(' ')))
-    df['letters'] = df['message'].apply(lambda s: len(s))
-    df["hour"] = df["datetime"].dt.hour
-    return df[["weekday", "datetime", "author", "message", "words", "letters", "hour"]]
+
+def _parse_messages(
+    text: str,
+    parser: Callable,
+    max_hour: int = MAX_HOUR,
+) -> List[Dict]:
+
+    parsed_messages = []
+    message_lines = []
+    for line in text.split("\n"):
+        line = line.strip()
+        if line == "":
+            continue
+        # Check for view once messages
+        if line[0].isdigit() and line.endswith(":"):
+            line = " ".join([line, VIEW_ONCE_FLAG])
+        # Check if next line is not a new message
+        if not parser(line):
+            # Check if this line is an info from WhatsApp
+            if date_dot(line) or date_slash(line):
+                continue
+            message_lines.append(line)
+            continue
+        # Check for first message
+        if not message_lines:
+            message_lines.append(line)
+            continue
+        parsed_message_dict = _parse_message(message_lines, parser)
+        parsed_messages.append(parsed_message_dict)
+        message_lines = [line]
+    # the last message needs to get parsed as well
+    parsed_message_dict = _parse_message(message_lines, parser)
+    parsed_messages.append(parsed_message_dict)
+    return parsed_messages
+
+
+def _parse_messages_from_file(
+    filepath: FilePath,
+    parser: Callable,
+    max_hour: int = MAX_HOUR,
+):
+
+    text: str
+    with open(filepath, "r", encoding="utf-8") as f:
+        text = f.read()
+    return _parse_messages(text, parser, max_hour)
+
+
+def _get_uncleaned_df_from_chat_log(
+    filepath: FilePath,
+    fmt: str,
+    max_hour: int = MAX_HOUR,
+) -> Union[pd.DataFrame, Tuple[pd.DataFrame, bool]]:
+
+    parser = compile(fmt).parse
+    parsed_messages = _parse_messages_from_file(
+        filepath, parser, max_hour
+    )
+    parsed_messages_df = pd.DataFrame(parsed_messages)
+    return parsed_messages_df
+
+
+def _get_df_from_chat_log_without_format(
+    filepath: FilePath,
+    max_hour: int = MAX_HOUR
+) -> Optional[pd.DataFrame]:
+
+    text: str
+    with open(filepath, "r", encoding="utf-8") as f:
+        text = f.read()
+
+    for fmt in FORMATS:
+        parser = compile(fmt)
+        if not parser.search(text):
+            continue
+
+        parsed_messages_df = _get_uncleaned_df_from_chat_log(
+            filepath, fmt, max_hour
+        )
+        max_day = parsed_messages_df.day.max()
+        max_month = parsed_messages_df.month.max()
+        if max_day > MAX_MONTH and max_month <= MAX_MONTH:
+            return parsed_messages_df
+
+        if max_day > MAX_MONTH or max_month <= MAX_MONTH:
+            print("Unambiguos date format, please specify file format.")
+            return None
+
+        parsed_messages_df.rename(columns={"day": "month", "month": "day"},
+                                  inplace=True)
+        return parsed_messages_df
+
+
+def get_df_from_chat_log(
+    filepath: FilePath,
+    fmt: Optional[str] = None,
+    max_hour: int = MAX_HOUR,
+) -> Optional[pd.DataFrame]:
+
+    if fmt is None:
+        parsed_messages_df = _get_df_from_chat_log_without_format(filepath, max_hour)
+    else:
+        parsed_messages_df = _get_uncleaned_df_from_chat_log(filepath, fmt, max_hour)
+
+    parsed_messages_df = parsed_messages_df.pipe(_df_cleaning)
+    return parsed_messages_df
+
+
+def _df_cleaning(
+    df: pd.DataFrame
+):
+
+    df = (
+        df.assign(messenger="wa")
+        .assign(date=pd.to_datetime(df[["year", "month", "day"]], errors="coerce"))
+    )
+    df = (
+        df.assign(datetime=df.date.astype(str) + ' ' + df.time.astype(str))
+        .drop(["day", "month", "year", "time", "date"], axis=1)
+        .astype(
+            {"author": "category", "messenger": "category", "datetime": "datetime64"}
+        )
+    )
+    return df
+
+
+def _message_length(
+    message: str
+) -> int:
+
+    message = message.replace(VIEW_ONCE_FLAG, "")
+    for flag in MEDIA_FLAGS:
+        message = message.replace(flag, "")
+    return len(message)
